146f64d78d2c9a2f65fd7e2985c7de29d6cabea7
Carlos Roberto Arias Arevalo <32964401+arias-spu@users.noreply.github.com>
2026-05-06 09:40:54 -0700

    in progress

--- product.cpp
+++ product.cpp
@@ -4,34 +4,53 @@
 
 #include "product.h"
 
+#include <sstream>
+
 Product::Product(unsigned int id, const std::string &name, float price, unsigned int quantity)
-    :_id(id), _name(name), _quantity(quantity){
-    if (price < 0)
-        _price = 0;
-    else
-        _price = price;
+    :_id(id), _name(name), _price(price < 0 ? 0 : price), _quantity(quantity){
+
 }
 
 std::string Product::GetName() const {
+    return _name;
 }
 
 unsigned int Product::GetId() const {
+    return _id;
 }
 
 float Product::GetPrice() const {
+    return _price;
 }
 
 void Product::SetQuantity(unsigned int quantity) {
+    _quantity = quantity;
 }
 
 unsigned int Product::GetQuantity() const {
+    return _quantity;
 }
 
 std::string Product::ToString() const {
+    std::stringstream ss;
+    ss << "ID: " << _id
+       << " Name: " << _name
+       << " Price: " << _price
+       << " Quantity: " << _quantity;
+    return ss.str();
 }
 
 void Product::Buy(unsigned int quantity) {
+    _quantity += quantity;
 }
 
 unsigned int Product::Sell(unsigned int quantity) {
+    if (quantity > _quantity) {
+        unsigned actualQuantity = _quantity;
+        _quantity = 0;
+        return actualQuantity;
+    }else {
+        _quantity -= quantity;
+        return quantity;
+    }
 }
